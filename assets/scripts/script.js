d8bc6229742ab3facc0cb886a0c6c8b02422d7f4
actuallyroy <amitkumar93525@gmail.com>
2022-09-08 21:15:06 +0530

    'roy'

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -241,6 +241,5 @@ function truncateString(str, num) {
 const bigImg = document.getElementById("bigImg")
 bigImg.src = "./assets/images/img.png";
 bigImg.onload = () => {
-  console.log(bigImg)
-  bigImg.style.animation = "fadeIn1 1000ms forwards";
+  bigImg.style.animation = "fadeIn1 1s 1s forwards";
 }
